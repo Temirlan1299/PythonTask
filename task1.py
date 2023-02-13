c7c9c1d0a46705323b4ee988ab725197bb6b4cda
Temirlan1299 <92721466+Temirlan1299@users.noreply.github.com>
2023-02-13 16:32:01 +0600

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,10 @@
+print("Hi all")
+print("My name is Temirlan")
+print("I am a student")
+a = 33
+b = 200
+#use if,else
+if b > a:
+  print("b is greater than a")
+else:
+    print("a is greater than b")
